c6697bd6525c51e123af1451b71c3964b8c25aa9
Ilya Verenchuk <lavgus@mail.ru>
2018-07-04 12:49:27 +0600

    Testing of factories

---
+++ Proba1/Proba1.test/OneArguments/FactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Proba1.OneArguments;
+using Proba1.test.OneArgumentsTests;
+using System;
+
+namespace Proba1.test.OneArguments
+{
+    public class FactoryTests
+    {
+        [TestCase("squareroot", typeof(SquareRootCalculator))]
+        [TestCase("squaring", typeof(SquaringCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentsFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
---
+++ Proba1/Proba1.test/TwoArguments/FactoryTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Proba1.test.TwoArgumentsTests;
+using System;
+
+namespace Proba1.test.TwoArguments
+{
+    public class FactoryTest
+    {
+        [TestCase("Add", typeof(AdditionCalculator))]
+        [TestCase("minus", typeof(SubtractionCalculator))]
+        [TestCase("Div", typeof(DivisionCalculator))]
+        [TestCase("multi", typeof(MultiplyCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
