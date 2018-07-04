5e84b106b6da7657287b2b934af69704a7daacbb
Ilya Verenchuk <lavgus@mail.ru>
2018-07-04 11:22:52 +0600

    create test for multiplication operation

--- Proba1/Proba1.test/TwoArgumentsTests/MultiplyCalculatorTests.cs
+++ Proba1/Proba1.test/TwoArgumentsTests/MultiplyCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Proba1.test.TwoArgumentsTests
 {
-    class MultiplyCalculatorTests
+    [TestFixture]
+    public class MultiplyCalculatorTests
     {
+        [Test]
+        public void TestCalculate()
+        {
+            ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator("multi");
+            double result = calculator.Calculate(2, 2);
+            Assert.AreEqual(4, result);
+        }
     }
 }
