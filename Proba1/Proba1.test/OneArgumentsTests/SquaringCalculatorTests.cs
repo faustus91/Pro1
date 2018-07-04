7754c032d18af6e831a8d167d26e7c3fa2fb829e
Ilya Verenchuk <lavgus@mail.ru>
2018-07-04 11:35:36 +0600

    creating a test for the operation squaring

--- Proba1/Proba1.test/OneArgumentsTests/SquaringCalculatorTests.cs
+++ Proba1/Proba1.test/OneArgumentsTests/SquaringCalculatorTests.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Proba1.test.OneArgumentsTests
-{
-    class SquaringCalculatorTests
+
     {
+
+        [TestFixture]
+        public class SquaringCalculatorTests
+
+        {
+            [Test]
+            public void TestCalculate()
+            {
+                IOneArgumentsCalculator calculator = OneArgumentsFactory.CreateCalculator("squaring");
+                double result = calculator.Calculate(4);
+                Assert.AreEqual(16, result);
+            }
+        }
     }
-}
