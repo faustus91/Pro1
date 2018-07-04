330e5208068bfe050631f15bcb724fafbdb39a75
Ilya Verenchuk <lavgus@mail.ru>
2018-07-04 11:42:00 +0600

    cleaning of the tests of extra derevatives

--- Proba1/Proba1.test/OneArgumentsTests/SquaringCalculatorTests.cs
+++ Proba1/Proba1.test/OneArgumentsTests/SquaringCalculatorTests.cs
@@ -1,15 +1,10 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Proba1.test.OneArgumentsTests
 
-    {
+{
 
-        [TestFixture]
+    [TestFixture]
         public class SquaringCalculatorTests
 
         {
