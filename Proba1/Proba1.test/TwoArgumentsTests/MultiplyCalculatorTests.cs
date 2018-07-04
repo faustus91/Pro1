a459d464432c27e81613f1d916ca604cdf3de979
Chernova_Alena <faustus91@mail.ru>
2018-07-04 12:22:04 +0600

    Complex tests for multiplication and subtraction

--- Proba1/Proba1.test/TwoArgumentsTests/MultiplyCalculatorTests.cs
+++ Proba1/Proba1.test/TwoArgumentsTests/MultiplyCalculatorTests.cs
@@ -5,12 +5,16 @@ namespace Proba1.test.TwoArgumentsTests
     [TestFixture]
     public class MultiplyCalculatorTests
     {
-        [Test]
-        public void TestCalculate()
+
+        [TestCase(0, 0, 0)]
+        [TestCase(3, 4, 12)]
+        [TestCase(-7, -2, 14)]
+
+        public void TestCalculate(double firstValue, double secondValue, double expected)
         {
             ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator("multi");
-            double result = calculator.Calculate(2, 2);
-            Assert.AreEqual(4, result);
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result);
         }
     }
 }
--- Proba1/Proba1.test/TwoArgumentsTests/SubtractionCalculatorTests.cs
+++ Proba1/Proba1.test/TwoArgumentsTests/SubtractionCalculatorTests.cs
@@ -5,12 +5,15 @@ namespace Proba1.test.TwoArgumentsTests
     [TestFixture]
     public class SubtractionCalculatorTests
     {
-        [Test]
-        public void TestCalculate()
+        [TestCase(0, 0, 0)]
+        [TestCase(3, 4, -1)]
+        [TestCase(-7, -3, -4)]
+
+        public void TestCalculate(double firstValue, double secondValue, double expected)
         {
             ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator("minus");
-            double result = calculator.Calculate(2, 2);
-            Assert.AreEqual(0, result);
+            double result = calculator.Calculate(firstValue, secondValue);
+            Assert.AreEqual(expected, result);
         }
     }
 }
